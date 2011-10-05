0b2d754ed11c537cba99d47542524f6a41410edb
Samuel Cole <sam@samuelcole.name>
2011-10-05 11:11:45 -0400

    Opera: trigger doc_html_updated

--- src/wymeditor/jquery.wymeditor.opera.js
+++ src/wymeditor/jquery.wymeditor.opera.js
@@ -108,4 +108,6 @@ WYMeditor.WymClassOpera.prototype.keyup = function(evt) {
   //'this' is the doc
   var wym = WYMeditor.INSTANCES[this.title];
   wym._selected_image = null;
+  $(wym._element).trigger('wymeditor:doc_html_updated', [wym, $(wym._doc.body).html()]);
 };
+
