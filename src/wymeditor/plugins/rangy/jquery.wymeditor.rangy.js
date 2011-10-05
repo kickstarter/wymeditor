1d7bdb4b5021e51d0ae050e155d9189cec9be68b
Samuel Cole <sam@samuelcole.name>
2011-10-05 11:16:49 -0400

    rangy: delint

--- src/wymeditor/plugins/rangy/jquery.wymeditor.rangy.js
+++ src/wymeditor/plugins/rangy/jquery.wymeditor.rangy.js
@@ -15,83 +15,98 @@
  *        Samuel Cole <sam@samuelcole.name>
  */
 
-(function($) {
-    if (WYMeditor && rangy) {
-      /* @name selection
-       * @description Returns a rangy selection node
-       */
-      WYMeditor.editor.prototype.selection = function() {
-        if(window.rangy && !rangy.initialized) rangy.init();
+(function ($) {
+  if (WYMeditor && rangy) {
+    /* @name selection
+     * @description Returns a rangy selection node
+     */
+    WYMeditor.editor.prototype.selection = function () {
+      if (window.rangy && !rangy.initialized) {
+        rangy.init();
+      }
 
-        var iframe = this._iframe;
-        var win = (iframe.contentDocument && iframe.contentDocument.defaultView) ?
-          iframe.contentDocument.defaultView : iframe.contentWindow;
-        var sel = rangy.getSelection(win);
+      var iframe = this._iframe,
+        win = (iframe.contentDocument && iframe.contentDocument.defaultView) ?
+          iframe.contentDocument.defaultView : iframe.contentWindow,
+        sel = rangy.getSelection(win);
 
-        return(sel);
-      };
+      return (sel);
+    };
 
-      /* @name selected
-       * @description Returns the selected node
-       */
-      WYMeditor.editor.prototype.selected = function() {
-        var sel = this.selection();
-        var node = sel.focusNode;
+    /* @name selected
+     * @description Returns the selected node
+     */
+    WYMeditor.editor.prototype.selected = function () {
+      var sel = this.selection(),
+        node = sel.focusNode;
 
-        if(node) {
-            if(node.nodeName == "#text") return(node.parentNode);
-            else return(node);
-        } else return(null);
-      };
+      if (node) {
+        if (node.nodeName === "#text") {
+          return (node.parentNode);
+        } else {
+          return (node);
+        }
+      } else {
+        return (null);
+      }
+    };
+    
+    WYMeditor.editor.prototype.remove_selection = function () {
+      var sel = this.selection();
+      sel.removeAllRanges();
+      sel.detach();
+    };
 
-      /* @name selection_collapsed
-       * @description Returns true/false if all selections are collapsed
-       */
-      WYMeditor.editor.prototype.selection_collapsed = function() {
-        var sel = this.selection();
-        var collapsed = false;
-        
-        $.each(sel.getAllRanges(), function() {
-          if(this.collapsed) {
-            collapsed = true;
-            //break
-            return false;
+    /* @name selection_collapsed
+     * @description Returns true/false if all selections are collapsed
+     */
+    WYMeditor.editor.prototype.selection_collapsed = function () {
+      var sel = this.selection(),
+        collapsed = false;
+      
+      $.each(sel.getAllRanges(), function () {
+        if (this.collapsed) {
+          collapsed = true;
+          //break
+          return false;
+        }
+      });
+
+      return (collapsed);
+    };
+    
+    /* @name selected_contains
+     * @description Returns an array of nodes that match a jQuery selector
+     * within the current selection.
+     */
+    WYMeditor.editor.prototype.selected_contains = function (selector) {
+      var sel = this.selection(),
+        matches = [];
+      
+      $.each(sel.getAllRanges(), function () {
+        $.each(this.getNodes(), function () {
+          if ($(this).is(selector)) {
+            matches.push(this);
           }
         });
+      });
 
-        return(collapsed);
-      };
-      
-      /* @name selected_contains
-       * @description Returns an array of nodes that match a jQuery selector
-       * within the current selection.
-       */
-      WYMeditor.editor.prototype.selected_contains = function(selector) {
-        var sel = this.selection();
-        var matches = [];
-        
-        $.each(sel.getAllRanges(), function() {
-          $.each(this.getNodes(), function() {
-            if($(this).is(selector)) {
-              matches.push(this);
-            }
-          });
-        });
+      return (matches);
+    };
 
-        return(matches);
-      };
+    /* @name selected_parents_contains
+     * @description Returns an array of nodes that match the selector within
+     * the selection's parents.
+     */
+    WYMeditor.editor.prototype.selected_parents_contains = function (selector) {
+      var $matches = $([]),
+        $selected = $(this.selected());
+      if ($selected.is(selector)) {
+        $matches = $matches.add($selected);
+      }
+      $matches = $matches.add($selected.parents(selector));
+      return ($matches);
+    };
 
-      /* @name selected_parents_contains
-       * @description Returns an array of nodes that match the selector within
-       * the selection's parents.
-       */
-      WYMeditor.editor.prototype.selected_parents_contains = function(selector) {
-        var $matches = $([]);
-        var $selected = $(this.selected());
-        if($selected.is(selector)) $matches = $matches.add($selected);
-        $matches = $matches.add($selected.parents(selector));
-        return($matches);
-      };
-
-    }
-})(jQuery);
+  }
+}(jQuery));
